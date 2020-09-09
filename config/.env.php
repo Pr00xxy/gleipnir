f02bca5d976ca893f85d7f65a360af8e51491ef5
Pr00xxy <hampus.westman@gmail.com>
2020-09-09 23:27:03 +0200

    add blackfire image and php configuration

--- config/.env.php
+++ config/.env.php
@@ -1,3 +1,7 @@
 
 PHP_IDE_CONFIG=serverName=dockerapp
 XDEBUG_CONFIG=remote_host=host.docker.internal idekey=PHPSTORM remote_mode=req remote_connect_back=0 remote_port=9001
+
+BLACKFIRE_LOG_LEVEL=4
+BLACKFIRE_SERVER_ID=""
+BLACKFIRE_SERVER_TOKEN=""
