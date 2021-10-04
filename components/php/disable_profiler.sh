1525eefe09be782b5d188331c01d04f6a3e35bbe
Pr00xxy <hampus.westman@gmail.com>
2021-10-04 13:32:50 +0200

    fix string literal in profiler toggle

--- components/php/disable_profiler.sh
+++ components/php/disable_profiler.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo -n "Turning xdebug off ... "
+echo -n "Turning profiler off ... "
 sed -i '/xdebug.profiler_enable=1/c\xdebug.profiler_enable=0' $COMPONENTS_PATH/php/${PHP_VERSION}/conf/php.ini
 sed -i '/xdebug.remote_autostart=1/c\xdebug.remote_autostart=0' $COMPONENTS_PATH/php/${PHP_VERSION}/conf/php.ini
 echo "done"
